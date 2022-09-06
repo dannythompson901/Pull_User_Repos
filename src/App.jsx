b23777d9a30f122ca4c12d8697d59c28d2987fd1
Danny <dannythompson901>
2022-09-05 21:57:38 -0500

    MR-1 Added functionality to call the Github API to pull list of repos for entered Username

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,47 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import poweredBy from "./powered-by-vitawind-dark.png";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [userName, setUserName] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  function handleSubmitRequest(e) {
+    e.preventDefault();
+    pullExistingRepos();
+  }
+
+  function pullExistingRepos() {
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${userName}/repos`,
+    }).then((res) => {
+      setRepos(res.data);
+    });
+  }
+
+  function listRepos(repo) {
+    return (
+      <div className="row" key={repo.id}>
+        <h4 className="repoName">{repo.name}</h4>
+      </div>
+    );
+  }
 
   return (
-    <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <img
-          src={logo}
-          className="animate-speed h-60 motion-safe:animate-spin"
-          alt="logo"
+    <div className="app ">
+      <form className="form flex flex-col justify-center">
+        <input
+          type="text"
+          value={userName}
+          placeholder="Enter Github Username"
+          onChange={(e) => setUserName(e.target.value)}
+          className="flex flex-col justify-center"
         />
-        <style>
-          {
-            "\
-            .animate-speed{\
-              animation-duration:20s;\
-            }\
-          "
-          }
-        </style>
-        <p className="bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text text-5xl font-black text-transparent selection:bg-transparent">
-          Vite + React + Tailwindcss v3
-        </p>
-        <p className="mt-3">
-          <button
-            type="button"
-            className="my-6 rounded bg-gray-300 px-2 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code className="text-[#8d96a7]">App.jsx</code> and save to test
-          HMR updates.
-        </p>
-        <p className="mt-3 flex gap-3 text-center text-[#8d96a7]">
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="text-[#61dafb] transition-all hover:text-blue-400"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-        <img src={poweredBy} className="mx-auto my-8" alt="powered-by" />
-      </header>
+        <button className="button" onClick={handleSubmitRequest}>
+          Submit
+        </button>
+      </form>
+      <div className="results">{repos.map(listRepos)}</div>
     </div>
   );
 }
