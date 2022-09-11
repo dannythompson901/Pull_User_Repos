a78341bd10eb3d2108d4228af6021c8a5a1fbe3f
Danny <dannythompson901>
2022-09-10 20:37:43 -0500

    MR-1 Added Tailwind Styling to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,10 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import Card from "./components/card";
 
 function App() {
-  const [userName, setUserName] = useState("");
-  const [repos, setRepos] = useState([]);
-
-  function handleSubmitRequest(e) {
-    e.preventDefault();
-    pullExistingRepos();
-  }
-
-  function pullExistingRepos() {
-    axios({
-      method: "get",
-      url: `https://api.github.com/users/${userName}/repos`,
-    }).then((res) => {
-      setRepos(res.data);
-    });
-  }
-
-  function listRepos(repo) {
-    return (
-      <div className="row" key={repo.id}>
-        <h4 className="repoName">{repo.name}</h4>
-      </div>
-    );
-  }
-
   return (
-    <div className="app ">
-      <form className="form flex flex-col justify-center">
-        <input
-          type="text"
-          value={userName}
-          placeholder="Enter Github Username"
-          onChange={(e) => setUserName(e.target.value)}
-          className="flex flex-col justify-center"
-        />
-        <button className="button" onClick={handleSubmitRequest}>
-          Submit
-        </button>
-      </form>
-      <div className="results">{repos.map(listRepos)}</div>
+    <div className="app">
+      <Card />
     </div>
   );
 }
